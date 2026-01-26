16d91ada9d7c23e559e0994d3ecc6bf2137fa97d
Brunopeace <137825826+Brunopeace@users.noreply.github.com>
2026-01-26 11:14:02 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,69 @@
+// ===============================
+// FORÇAR O SW NOVO A ASSUMIR O CONTROLE
+// ===============================
+self.addEventListener('install', (event) => {
+  self.skipWaiting(); // força ativação imediata
+
+  const CACHE_NAME = 'salgados-delicia-v1';
+  const urlsToCache = [
+      '/',
+      '/index.html',
+      '/css/cliente.css',
+      '/css/adm.css',
+      '/img/icon192.png',
+      '/img/icon512.png',
+      '/js/cliente.js',
+      '/js/adm.js'
+  ];
+
+  event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(cache => {
+            console.log('Service Worker: fazendo cache dos arquivos');
+            return cache.addAll(urlsToCache);
+        })
+  );
+});
+
+// ===============================
+// ATIVAÇÃO — REMOVE CACHE ANTIGO E ASSUME CONTROLE
+// ===============================
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+      caches.keys().then((cacheNames) => {
+          return Promise.all(
+              cacheNames.map((cache) => {
+                  if (cache !== 'gerenciador-de-clientes-v3') {
+                      console.log('Service Worker: removendo cache antigo:', cache);
+                      return caches.delete(cache);
+                  }
+              })
+          );
+      })
+  );
+
+  clients.claim(); // assume controle imediato das abas abertas
+});
+
+// ===============================
+// FETCH — ENTREGA DO CACHE + ONLINE
+// ===============================
+self.addEventListener('fetch', (event) => {
+
+  // 🔥 NÃO INTERCEPTAR PEDIDOS DO FIREBASE CLOUD MESSAGING
+  if (event.request.url.includes("fcm.googleapis.com")) {
+      return fetch(event.request); // deixa passar direto
+  }
+
+  event.respondWith(
+      caches.match(event.request)
+          .then((response) => {
+              if (response) {
+                  return response; // retorna do cache
+              }
+              console.log('Service Worker: buscando online:', event.request.url);
+              return fetch(event.request);
+          })
+          .catch(() => caches.match('/index.html'))
+  );
+});
