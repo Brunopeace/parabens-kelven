a6d02d3294f44b5b5d47f4679bb257d894ffcf77
Brunopeace <137825826+Brunopeace@users.noreply.github.com>
2026-01-26 11:31:06 -0300

    Atualizar o service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
 self.addEventListener('install', (event) => {
   self.skipWaiting(); // força ativação imediata
 
-  const CACHE_NAME = 'salgados-delicia-v1';
+  const CACHE_NAME = 'salgados-delicia-v2';
   const urlsToCache = [
       '/',
       '/index.html',
